233bf1c024d32d1ae097ca9fafe5b2811add8704
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-08 13:28:36 +0200

    csr ok

--- components/Nav.js
+++ components/Nav.js
@@ -3,11 +3,23 @@ import { NavLink } from './NavLink';
 export const Nav = () => {
     return (
         <nav>
-            <span><NavLink exact href="/">Welcome</NavLink></span>
+            <span>
+                <NavLink exact href="/">
+                    Welcome
+                </NavLink>
+            </span>
             <span className="separator">|</span>
-            <span><NavLink href="/code">Code</NavLink></span>
+            <span>
+                <NavLink href="/code">Code</NavLink>
+            </span>
             <span className="separator">|</span>
-            <span><NavLink href="/about">About</NavLink></span>
+            <span>
+                <NavLink href="/tech-items">Tech Items</NavLink>
+            </span>
+            <span className="separator">|</span>
+            <span>
+                <NavLink href="/about">About</NavLink>
+            </span>
         </nav>
     );
 };
